8e84976a6fe7d9b153696cc60d62b52ed70aa0b8
TeruyaInoue <94414747+TeruyaInoue@users.noreply.github.com>
2024-06-10 21:59:19 +0900

    新しいコマンド追加

--- commands/utils/getPlaylist.js
+++ commands/utils/getPlaylist.js
@@ -7,6 +7,7 @@ module.exports = {
     // コマンドの説明文
     .setDescription("クラブ配信のプレイリストのURLを表示します"),
   async execute(interaction) {
+    await interaction.deferReply({ ephemeral: true });
     const roles = interaction.member.roles.cache;
     const hasRole = roles.some((role) => role.name === "member");
     if (!hasRole) {
@@ -18,6 +19,6 @@ module.exports = {
     }
     const content =
       "全部:https://www.youtube.com/playlist?list=PLnJ5NkymnT04jtlp8W5LnTjeElu7pHbWk\nGK視点:https://www.youtube.com/playlist?list=PLnJ5NkymnT07k4HjHqqDmtGH9N0NENgln\n声有り:https://www.youtube.com/playlist?list=PLnJ5NkymnT07AScub7F8KMmGTmIpwSHYv\neS14th:https://www.youtube.com/playlist?list=PLnJ5NkymnT07dnDg3pCxyCdevRu07e_0X";
-    await interaction.reply({ content: content, ephemeral: true });
+    await interaction.editReply({ content: content, ephemeral: true });
   },
 };
