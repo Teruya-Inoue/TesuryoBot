cdd0727fef7fec6a475150f469887d69b124a368
TeruyaInoue <94414747+TeruyaInoue@users.noreply.github.com>
2024-06-16 04:31:33 +0900

    getVideoの月にaddChoices

--- commands/utils/getVideo.js
+++ commands/utils/getVideo.js
@@ -20,8 +20,20 @@ module.exports = {
       option
         .setName("month")
         .setDescription("月")
-        .setMinValue(1)
-        .setMaxValue(12)
+        .addChoices(
+          { name: "1", value: 1 },
+          { name: "2", value: 2 },
+          { name: "3", value: 3 },
+          { name: "4", value: 4 },
+          { name: "5", value: 5 },
+          { name: "6", value: 6 },
+          { name: "7", value: 7 },
+          { name: "8", value: 8 },
+          { name: "9", value: 9 },
+          { name: "10", value: 10 },
+          { name: "11", value: 11 },
+          { name: "12", value: 12 }
+        )
     )
     .addIntegerOption((option) =>
       option.setName("day").setDescription("日").setMinValue(1).setMaxValue(31)
