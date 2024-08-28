20d30ba33dd0f5bf0697ca4683b9895f5e2cd197
Teruya-Inoue <teruyapsgh@gmail.com>
2024-08-28 18:19:07 +0900

    フレマ追加の修正

--- commands/admin/addFriendlyMatch.js
+++ commands/admin/addFriendlyMatch.js
@@ -35,15 +35,35 @@ module.exports = {
           option
             .setName("goals")
             .setDescription("得点数")
-            .setRequired(true)
-            .setMinValue(0)
+            .addChoices(
+              { name: "0", value: 0 },
+              { name: "1", value: 1 },
+              { name: "2", value: 2 },
+              { name: "3", value: 3 },
+              { name: "4", value: 4 },
+              { name: "5", value: 5 },
+              { name: "6", value: 6 },
+              { name: "7", value: 7 },
+              { name: "8", value: 8 },
+              { name: "9", value: 9 }
+            )
         )
         .addIntegerOption((option) =>
           option
             .setName("opponentsgoals")
             .setDescription("被弾数")
-            .setRequired(true)
-            .setMinValue(0)
+            .addChoices(
+              { name: "0", value: 0 },
+              { name: "1", value: 1 },
+              { name: "2", value: 2 },
+              { name: "3", value: 3 },
+              { name: "4", value: 4 },
+              { name: "5", value: 5 },
+              { name: "6", value: 6 },
+              { name: "7", value: 7 },
+              { name: "8", value: 8 },
+              { name: "9", value: 9 }
+            )
         )
         .addIntegerOption((option) =>
           option
@@ -124,15 +144,35 @@ module.exports = {
           option
             .setName("goals")
             .setDescription("得点数")
-            .setRequired(true)
-            .setMinValue(0)
+            .addChoices(
+              { name: "0", value: 0 },
+              { name: "1", value: 1 },
+              { name: "2", value: 2 },
+              { name: "3", value: 3 },
+              { name: "4", value: 4 },
+              { name: "5", value: 5 },
+              { name: "6", value: 6 },
+              { name: "7", value: 7 },
+              { name: "8", value: 8 },
+              { name: "9", value: 9 }
+            )
         )
         .addIntegerOption((option) =>
           option
             .setName("opponentsgoals")
             .setDescription("被弾数")
-            .setRequired(true)
-            .setMinValue(0)
+            .addChoices(
+              { name: "0", value: 0 },
+              { name: "1", value: 1 },
+              { name: "2", value: 2 },
+              { name: "3", value: 3 },
+              { name: "4", value: 4 },
+              { name: "5", value: 5 },
+              { name: "6", value: 6 },
+              { name: "7", value: 7 },
+              { name: "8", value: 8 },
+              { name: "9", value: 9 }
+            )
         )
         .addIntegerOption((option) =>
           option
